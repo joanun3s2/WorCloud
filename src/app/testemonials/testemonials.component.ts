eb2a279bcfd296fbc2dddc2f796e555bd3475378
Isaac Beserra <isaac@qooling.com>
2024-04-26 16:08:50 -0300

    About, communities and testemonials components

---
+++ src/app/testemonials/testemonials.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-testemonials',
+  standalone: true,
+  imports: [],
+  templateUrl: './testemonials.component.html',
+  styleUrl: './testemonials.component.scss'
+})
+export class TestemonialsComponent {
+
+}
