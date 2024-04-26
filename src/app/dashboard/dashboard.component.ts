19bdbcf7adca7fb6e76a6728d89f1c6b666ed453
joao nunes <joanun3s2@gmail.com>
2024-04-26 15:34:36 -0300

    created layout file

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -1,13 +1,14 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { RouterLink, RouterModule } from '@angular/router';
+import { LayoutComponent } from '../../layout/app-layout.component';
 
 @Component({
   selector: 'app-dashboard',
   standalone: true,
   templateUrl: './dashboard.component.html',
   styleUrl: './dashboard.component.scss',
-  imports: [RouterModule],
+  imports: [RouterModule, LayoutComponent],
 })
 export class DashboardComponent {
   title = 'dashboard';
