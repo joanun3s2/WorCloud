0e9c00386574cb54729aeab355d0e863426a7f1b
joao nunes <joanun3s2@gmail.com>
2024-04-26 16:22:42 -0300

    New layout

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -2,13 +2,14 @@ import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { RouterLink, RouterModule } from '@angular/router';
 import { LayoutComponent } from '../../layout/app-layout.component';
+import { ProfileComponent } from '../profile/profile.component';
 
 @Component({
   selector: 'app-dashboard',
   standalone: true,
   templateUrl: './dashboard.component.html',
   styleUrl: './dashboard.component.scss',
-  imports: [RouterModule, LayoutComponent],
+  imports: [RouterModule, LayoutComponent, ProfileComponent],
 })
 export class DashboardComponent {
   title = 'dashboard';
